1980a6827d5dec5f7164a8174d65af780c9fc695
pansz <201101836@qq.com>
2022-04-26 15:50:11 +0800

    gitTest333333333

--- src/main/java/com/example/jenkinstest/contro/GitTest.java
+++ src/main/java/com/example/jenkinstest/contro/GitTest.java
@@ -3,6 +3,7 @@ package com.example.jenkinstest.contro;
 public class GitTest {
     public String getGit(){
         System.out.println("2222222222222222");
+        System.out.println("333333333333333");
         return "111111111";
     }
 }
