d93c728e669c12e695abd5abd5e25db364eac333
lty201101836 <89968711+lty201101836@users.noreply.github.com>
2022-04-26 17:12:43 +0800

    ========pull======
    ========pull======

--- src/main/java/com/example/jenkinstest/contro/Hello.java
+++ src/main/java/com/example/jenkinstest/contro/Hello.java
@@ -2,6 +2,7 @@ package com.example.jenkinstest.contro;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("========pull======");
         System.out.println("hdkhdfdjdkjdlkdlkdlk");
     }
 }
