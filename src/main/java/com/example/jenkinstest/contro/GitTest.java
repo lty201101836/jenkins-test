316c28455aeaf4f4a14b53826b9d0dd97675f172
pansz <201101836@qq.com>
2022-04-26 16:50:23 +0800

    push test

--- src/main/java/com/example/jenkinstest/contro/GitTest.java
+++ src/main/java/com/example/jenkinstest/contro/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push test");
         return "111111111";
     }
 }
